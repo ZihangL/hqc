ad84dac2ff05a05f045d9177df74ecc7c7f38e74
Zihang Li <lzh@iphy.ac.cn>
2024-11-02 10:51:47 +0800

    update test_pyscf pyscf_solver

--- test/test_pyscf.py
+++ test/test_pyscf.py
@@ -16,10 +16,11 @@ basis = 'gth-dzv'
 xc = 'lda,vwn'
 rcut = 24
 grid_length = 0.12
-smearing_sigma = 0.1
 key = jax.random.PRNGKey(42)
 gamma = False
 smearing = True
+smearing_method = 'fermi'
+smearing_sigma = 0.1
 
 key_p, key_kpt = jax.random.split(key)
 xp = jax.random.uniform(key_p, (n, 3), minval=0., maxval=L)
@@ -28,22 +29,25 @@ if gamma:
 else:
     kpoint = jax.random.uniform(key_kpt, (3,), minval=-jnp.pi/L/rs, maxval=jnp.pi/L/rs)
 
-def pyscf_hf(n: int, 
-             L: float, 
-             rs: float, 
-             xp: jnp.ndarray, 
-             kpt: jnp.ndarray, 
-             basis: str, 
-             smearing: bool = False,
-             smearing_method: str = 'fermi', 
-             smearing_sigma: float = 0.1, 
-             hf0: bool = False, 
-             silent: bool = True
-    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, 
-               np.ndarray, np.ndarray, np.ndarray, np.ndarray, 
-               float, float, float, float, float, float]:
+def pyscf_solver(n: int, 
+                 L: float, 
+                 rs: float, 
+                 xp: jnp.ndarray, 
+                 kpt: jnp.ndarray, 
+                 basis: str, 
+                 ifdft: bool = False,
+                 xc: str = 'lda,vwn',
+                 smearing: bool = False,
+                 smearing_method: str = 'fermi', 
+                 smearing_sigma: float = 0.1, 
+                 hf0: bool = False, 
+                 pyscf_verbose: int = 0,
+                 silent: bool = True
+            ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, 
+                       np.ndarray, np.ndarray, np.ndarray, np.ndarray, 
+                       float, float, float, float, float, float]:
     """
-        Pyscf Hartree Fock solver for hydrogen.
+        Pyscf Hartree Fock / DFT solver for hydrogen.
     INPUT:
         n: number of protons.
         L: side length of unit cell, unit: rs.
@@ -52,10 +56,13 @@ def pyscf_hf(n: int,
         sigma: smearing width, unit: Hartree.
         kpt: k-point, array of shape (3,).
         basis: gto basis name, eg:'gth-szv', 'gth-tzv2p', 'gth-qzv3p'.
+        ifdft: if True, use DFT solver, otherwise use Hartree Fock solver.
+        xc: exchange correlation functional, eg:'lda,vwn', only for .
         smearing: if True, use smearing (finite temperature Hartree Fock or thermal Hartree Fock).
         smearing_method: Fermi-Dirac smearing method, eg:'fermi', 'gaussian'.
         smearing_sigma: smearing width, unit: Hartree.
         hf0: if True, do Hartree Fock scf without Vee.
+        pyscf_verbose: verbose level of pyscf.
         silent: if True, suppress output.
     OUTPUT:
         pyscf_ovlp: overlap matrix, real or complex array of shape (n_ao, n_ao).
@@ -90,18 +97,19 @@ def pyscf_hf(n: int,
     cell.build()
 
     kpt = [kpt.tolist()]
-    kmf = scf.hf.RHF(cell, kpt=kpt)
+    if ifdft:
+        kmf = dft.RKS(cell, kpt=kpt)
+        kmf.xc = xc
+    else:
+        kmf = scf.hf.RHF(cell, kpt=kpt)
     # kmf.diis = False
     kmf.init_guess = '1e'
     if hf0:
         kmf.max_cycle = 1
         kmf.get_veff = lambda *args, **kwargs: np.zeros(kmf.get_hcore().shape)
     if smearing:
-        kmf = scf.addons.smearing_(kmf, sigma=sigma, method=smearing_method)
-    if silent:
-        kmf.verbose = 0
-    else:
-        kmf.verbose = 4
+        kmf = scf.addons.smearing_(kmf, sigma=smearing_sigma, method=smearing_method)
+    kmf.verbose = pyscf_verbose
     kmf.kernel()
 
     if not silent:
@@ -150,7 +158,7 @@ def pyscf_hf(n: int,
         # print("dm:\n", dm)
 
         # energy bands
-        bands = kmf.get_bands(kpts_band=kpt, kpt=kpt)[0][0]
+        bands = kmf.get_bands(kpts_band=kpt, kpt=np.array(kpt))[0][0]
         print(f"{GREEN}bands.shape:{RESET}", bands.shape)
         print(f"{GREEN}bands (Hartree):{RESET}", bands)
         print(f"{GREEN}bands (Rydberg):{RESET}", bands * Ry)
@@ -192,72 +200,13 @@ def pyscf_hf(n: int,
            mo_coeff, dm, bands*Ry, occ, \
            Eelec*Ry, Ecore*Ry, Vee*Ry, Vpp*Ry, Se, Etot*Ry
 
-def pyscf_dft(n, L, rs, sigma, xp, basis, kpt, xc='lda,vwn', smearing=False, smearing_method='fermi'):
-    """
-        Pyscf DFT solver for hydrogen.
-    INPUT:
-        n: number of protons.
-        L: side length of unit cell, unit: rs.
-        rs: average atomic spacing, unit: Bohr
-        sigma: smearing width, unit: Hartree.
-        xp: array of shape (n, dim), position of protons.
-        basis: gto basis name, eg:'gth-szv', 'gth-tzv2p', 'gth-qzv3p'.
-        kpt: k-point, array of shape (3,).
-        xc: exchange correlation functional, eg:'lda', 'pbe', 'b3lyp'.
-        smearing: if True, use Fermi-Dirac smearing
-            (finite temperature Hartree Fock or thermal Hartree Fock).
-    OUTPUT:
-        mo_coeff: molecular orbitals coefficients, complex array of shape (n_ao, n_mo).
-        bands: energy bands of corresponding molecular orbitals, 
-            ranking of energy from low to high. array of shape (n_mo,).
-    """
-    Ry = 2
-    xp *= rs
-    cell = gto.Cell()
-    cell.unit = 'B'
-    cell.a = np.eye(3) * L * rs
-    cell.atom = []
-    for ie in range(n):
-        cell.atom.append(['H', tuple(xp[ie])])
-    cell.spin = 0
-    cell.basis = {'H':gto.parse(load_as_str('H', basis), optimize=True)}
-    cell.build()
-
-    kpt = [kpt.tolist()]
-    kmf = dft.RKS(cell, kpt=kpt)
-    if smearing:
-        kmf = scf.addons.smearing_(kmf, sigma=sigma, method=smearing_method)
-    kmf.xc = xc
-    # kmf.diis = False
-    kmf.verbose = 0
-    kmf.kernel()
-    mo_coeff = kmf.mo_coeff  # (n_ao, n_mo)
-    bands = kmf.get_bands(kpts_band=kpt, kpt=np.array(kpt))[0][0]
-    E = kmf.e_tot - kmf.energy_nuc()
-
-    # print("dir(kmf):", dir(kmf))
-    # pyscf_ovlp = kmf.get_ovlp(kpt=kpt)[0]
-    # print("pyscf overlap.shape:\n", pyscf_ovlp.shape)
-    # print("pyscf overlap:\n", pyscf_ovlp)
-    # pyscf_hcore = kmf.get_hcore(kpt=kpt)[0]
-    # print("pyscf hcore.shape:\n", pyscf_hcore.shape)
-    # print("pyscf hcore:\n", pyscf_hcore)
-    # pyscf_veff = kmf.get_veff(kpt=kpt)
-    # print("pyscf veff.shape:\n", pyscf_veff.shape)
-    # print("pyscf veff:\n", pyscf_veff)
-    # pyscf_dm = kmf.make_rdm1(kpt=kpt)
-    # print("pyscf dm.shape:\n", pyscf_dm.shape)
-    # print("pyscf dm:\n", pyscf_dm)
-    # pyscf_j= kmf.get_j(kpt=kpt)
-    # print("pyscf j.shape:\n", pyscf_j.shape)
-    # print("pyscf j:\n", pyscf_j)
-    # pyscf_fock = kmf.get_fock()
-    # print("pyscf fock.shape:\n", pyscf_fock.shape)
-    # print("pyscf fock:\n", pyscf_fock)
-    # pyscf_k = 2 * (pyscf_hcore + pyscf_j - pyscf_fock)
-    # print("pyscf k.shape:\n", pyscf_k.shape)
-    # print("pyscf k:\n", pyscf_k)
-
 if __name__ == "__main__":
-    pyscf_hf(n, L, rs, xp, kpoint, basis, smearing=smearing, sigma=smearing_sigma, silent=False)
-    # pyscf_dft(n, L, rs, smearing_sigma, xp, basis, kpoint, xc=xc, smearing=smearing)
+    # HF
+    pyscf_solver(n, L, rs, xp, kpoint, basis, ifdft=False, xc=xc,
+                 smearing=smearing, smearing_method=smearing_method, 
+                 smearing_sigma=smearing_sigma, silent=False)
+    
+    # DFT
+    pyscf_solver(n, L, rs, xp, kpoint, basis, ifdft=True, xc=xc,
+                 smearing=smearing, smearing_method=smearing_method, 
+                 smearing_sigma=smearing_sigma, silent=False)
